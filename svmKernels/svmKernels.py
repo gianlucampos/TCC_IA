317e75d0304eebae02baf5876c1df5f91289208b
Gianlucampos <gianlucampos97@gmail.com>
2018-11-13 13:10:55 -0200

    Svm só decora as informações --Fazer validações (cross,k-fold) --Utilizar diferentes kernels e avaliar resultados

--- svmKernels/svmKernels.py
+++ svmKernels/svmKernels.py
@@ -2,7 +2,10 @@ import warnings
 import platform
 import pandas as pd
 import numpy as np
-from sklearn import svm
+from sklearn import svm, utils
+from sklearn.preprocessing import LabelEncoder
+from matplotlib import pyplot as plt
+
 
 warnings.filterwarnings("ignore")
 pathWindows = 'C:/Users/Pc/PycharmProjects/TCC/dataset/'
@@ -10,19 +13,27 @@ pathLinux = '/home/gianluca/Documentos/Projetos/Pycharm Projects/TCC_IA/dataset/
 path = pathLinux if platform.system() == 'Linux' else pathWindows
 dataset_Train = pd.read_csv(path + 'pdbbind-2007-refined-core-yx36i.csv')
 dataset_Test = pd.read_csv(path + 'pdbbind-2007-core-yx36i.csv')
+le = LabelEncoder()
+le.fit(dataset_Train['PDB'].astype(str))
+dataset_Train['PDB'] = le.transform(dataset_Train['PDB'].astype(str))
+le.fit(dataset_Test['PDB'].astype(str))
+dataset_Test['PDB'] = le.transform(dataset_Test['PDB'].astype(str))
 # =========================================================================================
 # Treinamento: Prevendo complexo de acordo com parâmetros passados:
 print('=' * 240)
 print('TREINAMENTO, sem validação (decorando)')
-params = dataset_Train.iloc[0:1105, :37]
-labels = dataset_Train.iloc[0:1105, 37:]
+params, labels = dataset_Train.loc[:, dataset_Train.columns != 'pbindaff'], dataset_Train.loc[:, 'pbindaff']
 x = np.array(params.values)
 y = np.array(labels.values.tolist())
 
 print('Informação: ', x[0].tolist())
 print('Resposta Correta: ', y[0])
 
-clf = svm.SVC(kernel='linear', C=100.0)
+y_encoded = le.fit_transform(y)
+
+clf = svm.SVC(kernel='rbf', C=1.0, gamma=0.1)
+# clf = svm.SVC(kernel='linear', C=1.0, gamma=0.1)
+# clf = svm.SVC(kernel='poly', C=1.0, gamma=0.1)
 clf.fit(x, y)
 print('Predição: ', clf.predict([x[0]]))
 print('Score: ', clf.score(x, y))
@@ -34,8 +45,7 @@ print('Score: ', clf.score(x, y))
 # Teste:
 print('=' * 240)
 print('TESTE')
-params_test = dataset_Test.iloc[0:1105, :37]
-labels_test = dataset_Test.iloc[0:1105, 37:]
+params_test, labels_test = dataset_Test.loc[:, dataset_Test.columns != 'pbindaff'], dataset_Test.loc[:, 'pbindaff']
 x_test = np.array(params_test.values)
 y_test = np.array(labels_test.values.tolist())
 
--- svmKernels/svmKernels_exemplo.py
+++ svmKernels/svmKernels_exemplo.py
@@ -2,44 +2,62 @@ import warnings
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from sklearn import svm, metrics, linear_model
 from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
 import platform
+from matplotlib import pyplot as plt
 
 warnings.filterwarnings("ignore")
 
 pathWindows = 'C:/Users/Pc/PycharmProjects/TCC/dataset/'
 pathLinux = '/home/gianluca/Documentos/Projetos/Pycharm Projects/TCC_IA/dataset/'
 path = pathLinux if platform.system() == 'Linux' else pathWindows
+dataset_Train = pd.read_csv(path + 'pdbbind-2007-refined-core-yx36i.csv')
+le = LabelEncoder()
+le.fit(dataset_Train['PDB'].astype(str))
+dataset_Train['PDB'] = le.transform(dataset_Train['PDB'].astype(str))
+# le.fit(dataset_Test['PDB'].astype(str))
+# dataset_Test['PDB'] = le.transform(dataset_Test['PDB'].astype(str))
 
-dataset = pd.read_csv(path + 'pdbbind-2007-refined-core-yx36i.csv')
-
-params = dataset.iloc[0:1105, :37]
-labels = dataset.iloc[0:1105, 37:]
+params, labels = dataset_Train.loc[:, dataset_Train.columns != 'pbindaff'], dataset_Train.loc[:, 'pbindaff']
 
 X_train, X_test, Y_train, Y_test = train_test_split(params, labels, train_size=0.5, random_state=1)
 
-svc = svm.SVC()
+svc = svm.SVR()
 svc.fit(X_train, Y_train)
 y_pred = svc.predict(X_test)
 print('Accuracy Score: ')
-print(metrics.accuracy_score(Y_test, y_pred))
+print(svc.score(Y_test, y_pred))
 
-svc = svm.SVC(kernel='linear')
-svc.fit(X_train, Y_train)
-y_pred = svc.predict(X_test)
-print('Accuracy Score : ')
-print(metrics.accuracy_score(Y_test, y_pred))
+lm = linear_model.LinearRegression()
+model = lm.fit(X_train, Y_train)
+predictions = lm.predict(X_test)
 
-svc = svm.SVC(kernel='rbf')
-svc.fit(X_train, Y_train)
-y_pred = svc.predict(X_test)
-print('Accuracy Score : ')
-print(metrics.accuracy_score(Y_test, y_pred))
+print(predictions[0:5])
+plt.scatter(Y_test, predictions)
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+plt.show()
+print("Score Before FIT:", model.score(X_test, Y_test))
 
-svc = svm.SVC(kernel='poly')
-svc.fit(X_train, Y_train)
-y_pred = svc.predict(X_test)
-print('Accuracy Score : ')
-print(metrics.accuracy_score(Y_test, y_pred))
+# print(metrics.accuracy_score(Y_test, y_pred))
 
+# svc = svm.SVC(kernel='linear')
+# svc.fit(X_train, Y_train)
+# y_pred = svc.predict(X_test)
+# print('Accuracy Score : ')
+# print(metrics.accuracy_score(Y_test, y_pred))
+#
+# svc = svm.SVC(kernel='rbf')
+# svc.fit(X_train, Y_train)
+# y_pred = svc.predict(X_test)
+# print('Accuracy Score : ')
+# print(metrics.accuracy_score(Y_test, y_pred))
+#
+# svc = svm.SVC(kernel='poly')
+# svc.fit(X_train, Y_train)
+# y_pred = svc.predict(X_test)
+# print('Accuracy Score : ')
+# print(metrics.accuracy_score(Y_test, y_pred))
+#
